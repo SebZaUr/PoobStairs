a713250c7945f691316ad4f08ddb1770453731c1
SebasZaUr <112453215+SebasZaUr@users.noreply.github.com>
2023-05-30 18:06:33 -0500

    "Falto lo tuyo"

--- src/domain/Pregunta.java
+++ src/domain/Pregunta.java
@@ -1,16 +1,41 @@
 package domain;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Pregunta {
 
-	private String pregunta;
-	private String respuesta;
+    private String pregunta;
+    private String[] respuestas;
+    private String respuestaCorrecta;
 
-	public String getPregunta() {
-		return null;
-	}
+    public Pregunta(String archivo) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(archivo))) {
+            String linea;
+            if ((linea = reader.readLine()) != null) {
+                String[] datos = linea.split(",");
+                pregunta = datos[0];
+                respuestas = new String[4];
+                for (int i = 0; i < 4; i++) {
+                    respuestas[i] = datos[i + 1];
+                }
+                respuestaCorrecta = datos[5];
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public String getRespuesta() {
-		return null;
-	}
+    public String getPregunta() {
+        return pregunta;
+    }
 
+    public String[] getRespuestas() {
+        return respuestas;
+    }
+
+    public String getRespuestaCorrecta() {
+        return respuestaCorrecta;
+    }
 }
--- src/presentation/PreguntaGUI.java
+++ src/presentation/PreguntaGUI.java
@@ -1,11 +1,61 @@
 package presentation;
 
+import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 
 import domain.Pregunta;
 
 public class PreguntaGUI extends JDialog {
-    public PreguntaGUI(Pregunta question) {
 
+    private String questionT, opA, opB, opC, opD;
+
+    public PreguntaGUI(Pregunta question) {
+        questionT = question.getPregunta();
+        String[] respuestas = question.getRespuestas();
+        opA = respuestas[0];
+        opB = respuestas[1];
+        opC = respuestas[2];
+        opD = respuestas[3];
+
+        JPanel panel = new JPanel();
+        panel.setOpaque(false);
+        getContentPane().add(panel, BorderLayout.NORTH);
+
+        JLabel pregunta = new JLabel(questionT);
+        panel.add(pregunta);
+
+        JPanel panel_1 = new JPanel();
+        panel_1.setOpaque(false);
+        getContentPane().add(panel_1, BorderLayout.CENTER);
+        panel_1.setLayout(new GridLayout(0, 1, 0, 0));
+
+        JButton OpcionA = new JButton(opA);
+        JButton OpcionB = new JButton(opB);
+        JButton OpcionC = new JButton(opC);
+        JButton OpcionD = new JButton(opD);
+
+        OpcionA.setFont(Estilos.FUENTE_LETRA);
+        OpcionB.setFont(Estilos.FUENTE_LETRA);
+        OpcionC.setFont(Estilos.FUENTE_LETRA);
+        OpcionD.setFont(Estilos.FUENTE_LETRA);
+
+        OpcionA.setBackground(Estilos.COLOR_LETRAS);
+        OpcionB.setBackground(Estilos.COLOR_LETRAS);
+        OpcionC.setBackground(Estilos.COLOR_LETRAS);
+        OpcionD.setBackground(Estilos.COLOR_LETRAS);
+
+        panel_1.add(OpcionA);
+        panel_1.add(OpcionB);
+        panel_1.add(OpcionC);
+        panel_1.add(OpcionD);
+
+        OpcionA.setFont(Estilos.FUENTE_LETRA);
+        OpcionA.setBackground(Estilos.COLOR_LETRAS);
     }
+
 }
+
